f480009e6660e17787ed266da179d032c2843733
finebot <kirill.baikin@gmail.com>
2019-08-17 13:44:08 +0300

    Updates

--- precache-manifest.75d9abd1c6e9332bdfe9187a02739ea6.js
+++ precache-manifest.75d9abd1c6e9332bdfe9187a02739ea6.js
@@ -20,15 +20,15 @@ self.__precacheManifest = [
     "url": "/see/static/js/runtime~main.ba59e697.js"
   },
   {
-    "revision": "e36b77e961c3376a6822",
-    "url": "/see/static/js/main.3303861d.chunk.js"
+    "revision": "ccf68628dd17df77f6be",
+    "url": "/see/static/js/main.89708996.chunk.js"
   },
   {
     "revision": "4a8755b32746a25642bc",
     "url": "/see/static/js/2.471b705e.chunk.js"
   },
   {
-    "revision": "e36b77e961c3376a6822",
+    "revision": "ccf68628dd17df77f6be",
     "url": "/see/static/css/main.a0e52915.chunk.css"
   },
   {
@@ -36,7 +36,7 @@ self.__precacheManifest = [
     "url": "/see/static/css/2.ad123a2d.chunk.css"
   },
   {
-    "revision": "0bb8f2cdf4a4e531afb0bacc6148f561",
+    "revision": "18fac1a8a77a204b1eb7cccc223a12e6",
     "url": "/see/index.html"
   }
 ];
