a7442a8fdad746c778bef6e84990fb18bee0f922
finebot <kirill.baikin@gmail.com>
2019-08-19 10:36:45 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/see/precache-manifest.210e93bfa329b6e9f1157e2f7916b48b.js"
+  "/see/precache-manifest.676efd6ac9e0ab3ee7a5fc05b1d4ef6a.js"
 );
 
 workbox.clientsClaim();
