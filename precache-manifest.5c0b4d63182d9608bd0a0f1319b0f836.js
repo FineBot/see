42d95959561b374edeb33ddf8ab60f99d1f45e77
finebot <kirill.baikin@gmail.com>
2019-08-25 12:34:08 +0300

    Updates

--- precache-manifest.5c0b4d63182d9608bd0a0f1319b0f836.js
+++ precache-manifest.5c0b4d63182d9608bd0a0f1319b0f836.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/see/static/media/ic_launcher.bc7158f2.png"
   },
   {
-    "revision": "0e709c65305b2d3116d1",
+    "revision": "49e7d133a37aabf5d81f",
     "url": "/see/static/css/main.9ce4fe58.chunk.css"
   },
   {
@@ -16,16 +16,16 @@ self.__precacheManifest = [
     "url": "/see/static/media/l6AtKw.4a4a9b10.png"
   },
   {
-    "revision": "6c6eb4f65b51d25c8d83",
-    "url": "/see/static/js/2.b14513a5.chunk.js"
+    "revision": "85e872f1b998aa1c3446",
+    "url": "/see/static/js/2.3ac2a656.chunk.js"
   },
   {
     "revision": "4e1ec8403d903dc514271d7328fbdeb3",
     "url": "/see/static/media/persik.4e1ec840.png"
   },
   {
-    "revision": "0e709c65305b2d3116d1",
-    "url": "/see/static/js/main.a998ae3c.chunk.js"
+    "revision": "49e7d133a37aabf5d81f",
+    "url": "/see/static/js/main.51eae389.chunk.js"
   },
   {
     "revision": "7a8ddf8b5089cda2b6acbf70136c03e7",
@@ -40,11 +40,11 @@ self.__precacheManifest = [
     "url": "/see/static/media/1LZwR4l6AtKw.4a916eec.png"
   },
   {
-    "revision": "6c6eb4f65b51d25c8d83",
+    "revision": "85e872f1b998aa1c3446",
     "url": "/see/static/css/2.d6c9e2a0.chunk.css"
   },
   {
-    "revision": "f642de622e17b7c3e48f998b5ec7ab41",
+    "revision": "e67507b8188d0d7eca7d354911ec250a",
     "url": "/see/index.html"
   }
 ];
