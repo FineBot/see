4c7840c502d2a5ca6c03716026611677125d3465
finebot <kirill.baikin@gmail.com>
2019-08-26 12:51:07 +0300

    Updates

--- precache-manifest.2485eb150c9be7184b5c6b06e5fc7c19.js
+++ precache-manifest.2485eb150c9be7184b5c6b06e5fc7c19.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/see/static/media/ic_launcher.bc7158f2.png"
   },
   {
-    "revision": "2d069bfc824d49bfcd66",
+    "revision": "62a6c71e48612439ace7",
     "url": "/see/static/css/main.989b4a1f.chunk.css"
   },
   {
@@ -24,8 +24,8 @@ self.__precacheManifest = [
     "url": "/see/static/media/persik.4e1ec840.png"
   },
   {
-    "revision": "2d069bfc824d49bfcd66",
-    "url": "/see/static/js/main.3950d5eb.chunk.js"
+    "revision": "62a6c71e48612439ace7",
+    "url": "/see/static/js/main.73dffe33.chunk.js"
   },
   {
     "revision": "42446fd58b2b0e69d3946a36d437caea",
@@ -48,7 +48,7 @@ self.__precacheManifest = [
     "url": "/see/static/css/2.d6c9e2a0.chunk.css"
   },
   {
-    "revision": "7160f9380e5eedd3f88f1b6b1e7605f4",
+    "revision": "f072e34ed05caf5b7e92e52c1e9ae371",
     "url": "/see/index.html"
   }
 ];
