1d6df6c6649cfa50753f73008fb7c8647e216d9c
finebot <kirill.baikin@gmail.com>
2019-09-01 12:39:48 +0300

    Updates

--- precache-manifest.4fe559d5c2a3205de199f7d5d1173969.js
+++ precache-manifest.4fe559d5c2a3205de199f7d5d1173969.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/see/static/media/ic_launcher.bc7158f2.png"
   },
   {
-    "revision": "ccda03787863b8630d9c",
+    "revision": "7bf271f5d478d2dacb57",
     "url": "/see/static/css/main.d1b73413.chunk.css"
   },
   {
@@ -24,8 +24,8 @@ self.__precacheManifest = [
     "url": "/see/static/media/persik.4e1ec840.png"
   },
   {
-    "revision": "ccda03787863b8630d9c",
-    "url": "/see/static/js/main.5a361c66.chunk.js"
+    "revision": "7bf271f5d478d2dacb57",
+    "url": "/see/static/js/main.2384b2b3.chunk.js"
   },
   {
     "revision": "42446fd58b2b0e69d3946a36d437caea",
@@ -48,7 +48,7 @@ self.__precacheManifest = [
     "url": "/see/static/css/2.d6c9e2a0.chunk.css"
   },
   {
-    "revision": "5de7ac59c1432d7ea644e880fb6569dd",
+    "revision": "459e48645ecd7a1e3650c6feb0a5e919",
     "url": "/see/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/see/precache-manifest.9301ee7c67ad70703a363f938038de0d.js"
+  "/see/precache-manifest.4fe559d5c2a3205de199f7d5d1173969.js"
 );
 
 workbox.clientsClaim();
