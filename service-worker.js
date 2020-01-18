4c376e5fe327432abd9721ce69bd72839cdf8260
FineBot <kirill.baikin@gmail.com>
2020-01-18 15:51:41 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/see/precache-manifest.8bab592f4ae15652a8260f946bf52e45.js"
+  "/see/precache-manifest.5e7391119ae5b258a200d761017586a2.js"
 );
 
 self.addEventListener('message', (event) => {
